895b136e26d565c77c8e4bc1e5e19ee690451b24
SimoneCappadona <simocappa95@gmail.com>
2024-02-07 14:30:15 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data (){
+        return{
+            title : 'Hello Vite, Hello World'
+        }
+    }
+}
+</script>
+
+<template>
+  <h1 class="textcolor">{{ title }}</h1>
+</template>
+
+<style>
+@import url(./style.css);
+</style>
